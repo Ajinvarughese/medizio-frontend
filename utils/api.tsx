44ddc3d6717fcf0acee6f540ffc149d95b236be0
Ajin Varughese <ajinvarughese91@gmail.com>
2026-03-15 23:17:57 +0530

    update

--- utils/api.tsx
+++ utils/api.tsx
@@ -2,7 +2,7 @@ import { Platform } from "react-native";
 
 const fetchUrl = () : string => {
     if(Platform.OS === 'web') return 'http://localhost:8080/api'
-    else return "http://10.70.161.237:8080/api";
+    else return "http://192.168.1.4:8080/api";
 } 
 
 const API_URL = fetchUrl();
